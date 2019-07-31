1de30c1454140bc5369d26b0c264e4071996a89c
Jason Lin <jlin10@email.wm.edu>
2019-07-30 21:33:17 -0400

    Updated code to iterate over all p tags after headings

--- iGemScraper.py
+++ iGemScraper.py
@@ -8,20 +8,23 @@ from bs4 import BeautifulSoup as soup
 	
 if __name__ == "__main__":		
 	
-	#TODO: Adapt to new format
+	
 	def write_file(dict, fileName):
-		with open(fileName, 'w', newline='') as csvfile:
+		with open(fileName, 'w', newline='', encoding = 'utf-8-sig') as csvfile:
 			writer = csv.writer(csvfile, delimiter=',')
 			#csvWriterDumb = [names[0][i]] + [names[1][i]] + [names[2][i]] + [names[3][i]]
 			#writer.writerow(csvWriterDumb)
 			for key, value in dict.items():
-				toArr = [key] + [value]
-				print(key, ':', value, "\n")
-				writer.writerow(toArr)
+				for key2, val2 in value.items():
+					toArr = [key] + [key2] + [val2]
+					print(key2, ':', val2, "\n")
+					writer.writerow(toArr)
 	
-	dictionary = {}
-
-	my_url = 'http://2017.igem.org/Team:MIT/outreach'
+	bigDict = {}
+	contentDictionary = {}
+	
+	site_name = "MIT"
+	my_url = 'http://2017.igem.org/Team:MIT/HP/Gold_Integrated'
 	uClient = uReq(my_url)
 	page_html = uClient.read()
 	uClient.close()
@@ -33,21 +36,32 @@ if __name__ == "__main__":
 	#print(style_tag)
 	
 	content = page_soup.find_all("body")[0]
-	headings = content.find_all(["h1","h2","h3","h4","h5"])
+	hTags = ["h1","h2","h3","h4","h5"]
+	pTags = ["p", "p1", "p2"]
+	headings = content.find_all(hTags)
 	#print(headings)
 	print (len(headings))
 	for heading in headings:
 		heading_text = heading.get_text()
-		next_tag = heading.find_next_sibling(["p","p1","p2"])
+		next_tag = heading.next_sibling
+		print(next_tag)
+		next_text = ""
+		while next_tag != None and next_tag.name not in hTags:
+			if(next_tag.string!=None) and next_tag.name in pTags:
+				next_text += next_tag.string
+			next_tag = next_tag.next_sibling
 		#print (countFreq("{", a))
-		if next_tag != None:
-			dictionary[heading_text] = next_tag.get_text()
-			print("/////////////////////////")
-			print (heading_text + "\n")
-			print (next_tag.get_text())
-	print(dictionary)
-	
-	write_file(dictionary, "MIT.csv")
+		# if next_tag != None:
+			# contentDictionary[heading_text] = next_tag.get_text()
+			# print("/////////////////////////")
+			# print (heading_text + "\n")
+			# print (next_tag.get_text())
+		if next_text != "":
+			contentDictionary[heading_text] = next_text
+		
+	bigDict[site_name] = dict(contentDictionary)
+	print(bigDict)
+	write_file(bigDict, "MIT.csv")
 		
 		
 		
